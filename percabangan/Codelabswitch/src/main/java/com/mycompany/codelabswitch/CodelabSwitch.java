93091e151706170b77ed8e788a476112d7bfd173
GazafaizMoebarrak <gmoebarrak@gmail.com>
2024-09-04 08:59:00 +0700

    Add files via upload

---
+++ percabangan/Codelabswitch/src/main/java/com/mycompany/codelabswitch/CodelabSwitch.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.codelabswitch;
+
+/**
+ *
+ * @author DH
+ */
+public class Codelabswitch {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ percabangan/SwitchwithouthBreak/src/main/java/com/mycompany/switchwithoutbreak/SwitchWithoutBreak.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.switchwithouthbreak;
+
+/**
+ *
+ * @author DH
+ */
+public class SwitchwithouthBreak {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
